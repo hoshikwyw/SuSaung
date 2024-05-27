c848ae6fb570d5ba4c48f9cdf0c216b5875db192
hoshikwyw <khaingwutyiwin1712@gmail.com>
2024-05-27 23:53:31 +0630

    Logic not done

--- src/common/CommonFn.js
+++ src/common/CommonFn.js
@@ -1,3 +1,26 @@
-const navBtnclick = (text) => { console.log(text, "nav bar is clicked!"); }
+import { createWorker } from "tesseract.js";
 
-export default navBtnclick
+const navBtnclick = (text) => {
+  console.log(text, "nav bar is clicked!");
+};
+
+const initializeTesseract = async () => {
+  const worker = createWorker({
+    logger: (m) => {
+      console.log(m);
+    },
+  });
+  await worker.load();
+  await worker.loadLanguage("eng");
+  await worker.initialize("eng");
+  return worker;
+};
+
+const recognizeImage = async (worker, ImageData) => {
+  const {
+    data: { text },
+  } = await worker.recognize(ImageData);
+  return text;
+};
+
+export default { navBtnclick, initializeTesseract, recognizeImage };
