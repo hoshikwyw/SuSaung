ab6dcb3d2a52b7408d67c9fc1e29c872744e5dd1
KhaingWutYiWin <113814930+hoshikwyw@users.noreply.github.com>
2024-05-28 16:42:15 +0630

    Upload Logic done

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,52 +1,32 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { createWorker } from 'tesseract.js';
+import React, { useState } from 'react';
+import { extractTextFromImage } from '../api/uploadApi'
 
 const Upload = () => {
-    const [ocr, setOcr] = useState("");
-    const [imageData, setImageData] = useState(null);
+    const [imageData, setImageData] = useState()
+    const [convertText, setConvertText] = useState()
+    const [textArr, setTextArr] = useState()
 
-    const convert = useCallback(async () => {
-        if (!imageData) return;
-
-        const worker = createWorker();
-
-        try {
-            await worker.load();
-            await worker.loadLanguage('eng');
-            await worker.initialize('eng');
-            const { data } = await worker.recognize(URL.createObjectURL(imageData));
-            setOcr(data.text);
-        } catch (error) {
-            console.error("Error performing OCR: ", error);
-        } finally {
-            await worker.terminate(); // Always terminate the worker after usage
-        }
-    }, [imageData]);
-
-    useEffect(() => {
-        convert();
-    }, [imageData, convert]);
-
-    const handleChange = (e) => {
-        if (e.target.files[0]) {
-            setImageData(e.target.files[0]);
-        } else {
-            setImageData(null);
-            setOcr("");
+    const handleConvert = async () => {
+        if (imageData) {
+            const res = await extractTextFromImage(imageData)
+            setConvertText(res.result_string)
+            setTextArr(res)
+            console.log(textArr);
         }
     }
 
     return (
         <div className=' h-full flex flex-col justify-center items-center gap-10'>
             <div className=' h-[30%] w-[98%] flex flex-col justify-center items-center subBg'>
-                <img src="/upload.png" alt="upload.png" className=' w-[60px]' />
-                <h1 className=' text-sm'>Upload Image</h1>
-                <input type="file" accept='image/*' onChange={handleChange} />
-                {imageData && <img src={URL.createObjectURL(imageData)} alt="preview" />}
+                <>
+                    <input type="file" accept='image/*' onChange={(e) => setImageData(e.target.files[0])} />
+                    <input type="button" value="Convert" onClick={handleConvert} />
+                </>
+
+                {imageData && <img src={URL.createObjectURL(imageData)} alt="preview" className=' w-[10%]' />}
             </div>
             <div className=' h-[70%] w-[98%] flex justify-center items-center subBg'>
-                <h1>scanning text ....</h1>
-                <p>{ocr}</p>
+                <p>{convertText}</p>
             </div>
         </div>
     );
@@ -55,83 +35,3 @@ const Upload = () => {
 export default Upload;
 
 
-// import React from 'react';
-// import Tesseract from 'tesseract.js';
-
-// import './style.css';
-
-// const App = () => {
-//   const [isLoading, setIsLoading] = React.useState(false);
-//   const [image, setImage] = React.useState('');
-//   const [text, setText] = React.useState('');
-//   const [progress, setProgress] = React.useState(0);
-
-//   const handleSubmit = () => {
-//     setIsLoading(true);
-//     Tesseract.recognize(image, 'eng', {
-//       logger: (m) => {
-//         console.log(m);
-//         if (m.status === 'recognizing text') {
-//           setProgress(parseInt(m.progress * 100));
-//         }
-//       },
-//     })
-//       .catch((err) => {
-//         console.error(err);
-//       })
-//       .then((result) => {
-//         console.log(result.data);
-//         setText(result.data.text);
-//         setIsLoading(false);
-//       });
-//   };
-
-//   return (
-//     <div className="container" style={{ height: '100vh' }}>
-//       <div className="row h-100">
-//         <div className="col-md-5 mx-auto h-100 d-flex flex-column justify-content-center">
-//           {!isLoading && (
-//             <h1 className="text-center py-5 mc-5">Image To Text</h1>
-//           )}
-//           {isLoading && (
-//             <>
-//               <progress className="form-control" value={progress} max="100">
-//                 {progress}%{' '}
-//               </progress>{' '}
-//               <p className="text-center py-0 my-0">Converting:- {progress} %</p>
-//             </>
-//           )}
-//           {!isLoading && !text && (
-//             <>
-//               <input
-//                 type="file"
-//                 onChange={(e) =>
-//                   setImage(URL.createObjectURL(e.target.files[0]))
-//                 }
-//                 className="form-control mt-5 mb-2"
-//               />
-//               <input
-//                 type="button"
-//                 onClick={handleSubmit}
-//                 className="btn btn-primary mt-5"
-//                 value="Convert"
-//               />
-//             </>
-//           )}
-//           {!isLoading && text && (
-//             <>
-//               <textarea
-//                 className="form-control w-100 mt-5"
-//                 rows="30"
-//                 value={text}
-//                 onChange={(e) => setText(e.target.value)}
-//               ></textarea>
-//             </>
-//           )}
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
